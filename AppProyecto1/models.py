984a265b45de9068ac20b10ea1cef24457702923
Esteban <e.napo_esteban@hotmail.com>
2023-09-21 23:04:48 -0300

    Agregadas correcciones en admin.py

--- AppProyecto1/models.py
+++ AppProyecto1/models.py
@@ -13,13 +13,19 @@ class Tarea (models.Model):
     estado = models.CharField(max_length=20, choices=ESTADO_CHOICES)
 
     def __str__(self):
-        return self.nombredetarea
+        return f'{self.nombredetarea} - {self.estado}'
     
 class Proyecto (models.Model):
     
     nombredeproyecto = models.CharField(max_length=40)
     descripcion = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return f'{self.nombredeproyecto} - {self.descripcion}'
+
 class Miembro (models.Model):
     nombre = models.CharField(max_length=40)
+
+    def __str__(self):
+        return f'{self.nombre}'
     
